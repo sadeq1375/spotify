e8b41170716800e9212f0a2521a989cc4dc6d5f6
sadeghweb <hashemisadegh59@yahoo.com>
2024-04-20 12:46:46 +0330

    fetching songs

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import SupabaseProvider from "./../providers/SupabaseProvider";
 import UserProvider from "@/providers/UserProvider";
 import ModalProvider from "@/providers/ModalProvider";
 import ToasterProvider from "@/providers/ToasterProvider";
+import getSongsByUserId from "@/actions/getSongsByUserId";
 
 const figtree = Figtree({ subsets: ["latin"] });
 
@@ -13,12 +14,14 @@ export const metadata: Metadata = {
   title: "Spotify",
   description: "Join the party🥳",
 };
+export const revalidate = 0;
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const userSongs = await getSongsByUserId();
   return (
     <html lang="en">
       <body className={figtree.className}>
@@ -26,7 +29,7 @@ export default function RootLayout({
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider />
-            <Sidebar>{children}</Sidebar>
+            <Sidebar songs={userSongs}>{children}</Sidebar>
           </UserProvider>
         </SupabaseProvider>
       </body>
--- components/Library.tsx
+++ components/Library.tsx
@@ -5,8 +5,13 @@ import { useUser } from "@/hooks/useUser";
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
 import useUploadModal from "./../hooks/useUploadModal";
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface LibraryProps {
+  songs: Song[];
+}
+const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
@@ -30,7 +35,11 @@ const Library = () => {
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
       </div>
-      <div className="flex flex-col gap-y-2 mt-2 px-3">List OF Songs</div>
+      <div className="flex flex-col gap-y-2 mt-2 px-3">
+        {songs.map((item) => (
+          <MediaItem onClick={() => {}} key={item.id} data={item} />
+        ))}
+      </div>
     </div>
   );
 };
