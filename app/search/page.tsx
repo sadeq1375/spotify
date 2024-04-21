41d62178cb9a4472f18ba8deb678dce6515f4b89
sadeghweb <hashemisadegh59@yahoo.com>
2024-04-21 16:15:11 +0330

    liked-songs

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,7 @@
 import getSongsByTitle from "@/actions/getSongsByTitle";
 import Header from "@/components/Header";
 import SearchInput from "@/components/SearchInput";
+import SearchContent from "./components/SearchContent";
 
 interface SearchProps {
   searchParams: {
@@ -18,6 +19,7 @@ const Search = async ({ searchParams }: SearchProps) => {
           <SearchInput />
         </div>
       </Header>
+      <SearchContent songs={songs} />
     </div>
   );
 };
--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -5,7 +5,7 @@ function useDebounce<T>(value: T, delay?: number): T {
   useEffect(() => {
     const timer = setTimeout(() => {
       setDebouncedValue(value);
-    }, delay || 500);
+    }, delay || 300);
     return () => {
       clearTimeout(timer);
     };
