0194b28bdf6a502a0bb8331b2bd673f1f6b12dd5
sadeghweb <hashemisadegh59@yahoo.com>
2024-04-21 13:33:32 +0330

    implementing search

---
+++ components/SearchInput.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const SearchInput = () => {
+  const router = useRouter();
+  const [value, setValue] = useState<string>("");
+  const debouncedValue = useDebounce<string>(value, 500);
+  return <div></div>;
+};
+
+export default SearchInput;
