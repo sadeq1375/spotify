f27ee8c6068296da547ae40abe75ae19fa49d1b1
sadeghweb <hashemisadegh59@yahoo.com>
2024-04-25 17:51:25 +0200

    sound wont reset

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,19 +10,21 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useUser } from "@/hooks/useUser";
 import { FaUserAlt } from "react-icons/fa";
 import toast from "react-hot-toast";
+import usePlayer from "@/hooks/usePlayer";
 
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
 }
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const player = usePlayer();
   const authModal = useAuthModal();
   const router = useRouter();
   const supabaseClient = useSupabaseClient();
   const { user } = useUser();
   const handleLogout = async () => {
     const { error } = await supabaseClient.auth.signOut();
-    //Todo Reset any playing songs
+    player.reset();
     router.refresh();
     if (error) {
       toast.error(error.message);
