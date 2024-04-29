2a4c833cc3831dab4193f13d6ee0473361156923
sadeghweb <hashemisadegh59@yahoo.com>
2024-04-29 22:56:40 +0330

    utils

---
+++ utils/reducer.js
@@ -0,0 +1,23 @@
+import { reducerCases } from "./constans";
+
+export const initialState = {
+  token: null,
+  playlists: [],
+  userInfo: null,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case reducerCases.SET_TOKEN: {
+      return { ...state, token: action.token };
+    }
+    case reducerCases.SET_PLAYLISTS: {
+      return { ...state, playlists: action.playlists };
+    }
+    case reducerCases.SET_USER: {
+      return { ...state, userInfo: action.userInfo };
+    }
+    default:
+      return state;
+  }
+};
+export default reducer;
